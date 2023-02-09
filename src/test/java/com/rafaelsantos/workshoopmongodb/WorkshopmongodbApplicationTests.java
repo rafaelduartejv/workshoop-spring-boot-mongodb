b23a93791bcad073b5d26c8f149d160fecd10353
rafaelduartejv <rafaelduarte.jv@gmail.com>
2023-02-09 19:35:48 -0300

    First Commit - Project created

---
+++ src/test/java/com/rafaelsantos/workshoopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.workshoopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
