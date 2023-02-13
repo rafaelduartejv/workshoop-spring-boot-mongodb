0c80d806ffad0c78ada6bed6aa19b3699fc7a8c9
rafaelduartejv <rafaelduarte.jv@gmail.com>
2023-02-12 21:07:42 -0300

    Getting user by ID

--- src/main/java/com/rafaelsantos/workshoopmongodb/resources/UserResources.java
+++ src/main/java/com/rafaelsantos/workshoopmongodb/resources/UserResources.java
@@ -5,9 +5,7 @@ import com.rafaelsantos.workshoopmongodb.dto.UserDTO;
 import com.rafaelsantos.workshoopmongodb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,5 +22,10 @@ public class UserResources {
         List<User> list = userService.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+    @RequestMapping(value="/{id}", method= RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
---
+++ src/main/java/com/rafaelsantos/workshoopmongodb/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.rafaelsantos.workshoopmongodb.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+
+
+}
